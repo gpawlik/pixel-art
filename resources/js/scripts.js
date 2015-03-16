cd6fb101aa01dfd078adbcff9f1abc0700e60f14
gpawlik <pawlik.gregor@gmail.com>
2015-03-16 19:07:58 +0100

    Code refactoring.

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -50,6 +50,43 @@ $(function() {
 
     function add_new_color(color){
         $color_box.append('<li style="background:#' + color + '"></li>');
-    }                                               
+    }  
+    
+    initImageUploading();
 
 });
+
+
+// Image uploading
+function initImageUploading() {
+    var upload = document.getElementsByTagName('input')[0],
+        holder = document.getElementById('uploaded-image'),
+        state  = document.getElementById('status');
+
+    if (typeof window.FileReader === 'undefined') {
+        state.className = 'fail';
+    } else {
+        state.className = 'success';
+        state.innerHTML = 'File API & FileReader available';
+    }
+
+    upload.onchange = function (e) {
+        e.preventDefault();
+
+        var file = upload.files[0],
+        reader = new FileReader();
+        reader.onload = function (event) {
+            var img = new Image();
+            img.src = event.target.result;
+            // note: no onload required since we've got the dataurl...I think! :)
+            if (img.width > 560) { // holder width
+                img.width = 560;
+            }
+            holder.innerHTML = '';
+            holder.appendChild(img);
+        };
+        reader.readAsDataURL(file);
+
+        return false;
+    };    
+}
